53ac4648d1550d42b961faa44a664c11b44a0615
admin01 <paradoxraphael@gmai.com>
2024-06-05 17:43:14 +1000

    adding props

--- 01_pizza_menu/src/index.js
+++ 01_pizza_menu/src/index.js
@@ -67,12 +67,36 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
   );
 }
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
 function Footer() {
   const hours = new Date().getHours();
   console.log(hours);
@@ -87,15 +111,7 @@ function Footer() {
     <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>
   );
 }
-function Pizza() {
-  return (
-    <div className="footer">
-      <img src="pizzas/spinaci.jpg" alt="spinaci.jpg" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // render things twice
