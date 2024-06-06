19861672ae77ae511f72a96e824d193d9f3abd62
admin01 <paradoxraphael@gmai.com>
2024-06-06 11:23:58 +1000

    challenge 01-01

---
+++ challenge_01_01/src/index.js
@@ -0,0 +1,61 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="logo512.png" alt="logo" />;
+}
+
+function Intro() {
+  return (
+    <div className="data">
+      <h1>John Doe</h1>
+      <p>
+        Developed scalable web applications. Integrated RESTful APIs.
+        Implemented state management solutions.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" emoji="💪" color="blue" />
+      <Skill skill="Javascript" emoji="💪" color="orange" />
+      <Skill skill="React" emoji="💪" color="red" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
