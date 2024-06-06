aa0a7641721049c0f54f3d13f5eb57fea9c566f6
admin01 <paradoxraphael@gmai.com>
2024-06-06 13:32:17 +1000

    destructing the props and use fragment

--- 01_pizza_menu/src/index.js
+++ 01_pizza_menu/src/index.js
@@ -69,12 +69,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {pizzas.length > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObject={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our menu. Please come back later.</p>
       )}
@@ -95,17 +102,17 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObject.soldOut) {
-    return null;
-  }
+function Pizza({ pizzaObject }) {
+  // if (pizzaObject.soldOut) {
+  //   return null;
+  // }
   return (
-    <li className="pizza">
-      <img src={props.pizzaObject.photoName} alt={props.pizzaObject.name} />
+    <li className={`pizza ${pizzaObject.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <div>
-        <h3>{props.pizzaObject.name}</h3>
-        <p>{props.pizzaObject.ingredients}</p>
-        <span>{props.pizzaObject.price}</span>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        <span>{pizzaObject.soldOut ? "Sold Out" : pizzaObject.price}</span>
       </div>
     </li>
   );
@@ -124,17 +131,26 @@ function Footer() {
   return (
     <footer>
       {isOpen ? (
-        <div className="order">
-          <p>We are open until {closeHour}:00. Come visit us or order online</p>
-
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We are happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
       )}
     </footer>
+  );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We are open from {openHour}:00 until {closeHour}:00. Come visit us or
+        order online
+      </p>
+
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
