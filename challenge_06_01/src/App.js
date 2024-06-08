78ffb4856206c8370b72a7b04dfe5518641692d3
admin01 <paradoxraphael@gmai.com>
2024-06-08 11:37:04 +1000

    challenge 06_01

---
+++ challenge_06_01/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function decStep() {
+    if (step >= 2) {
+      setStep((s) => s - 1);
+    }
+  }
+  function incCount() {
+    setCount((s) => s + step);
+  }
+  function decCount() {
+    setCount((s) => s - step);
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div>
+        <button onClick={decStep}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={decCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={incCount}>+</button>
+      </div>
+      <p>
+        {count} days from today is {date.toDateString()}
+      </p>
+    </div>
+  );
+}
+export default App;
