3fd46681aaea5f583000758de35674e590d1bb27
admin01 <paradoxraphael@gmai.com>
2024-06-10 09:35:52 +1000

    add early return with stats component

--- 03_far_away/src/App.js
+++ 03_far_away/src/App.js
@@ -104,13 +104,19 @@ function Item({ item, handleDeleteItems, handleToggleItem }) {
   );
 }
 function Stats({ packList }) {
+  if (!packList.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to you packing list 🚀</em>
+      </p>
+    );
   const itemNumber = packList.length;
   const itemPacked = packList.filter((i) => i.packed).length;
   return (
     <footer className="stats">
       <em>
         💼You have {itemNumber} items on your list, and you have packed{" "}
-        {itemPacked} item({(itemPacked / itemNumber).toFixed(2)}%).
+        {itemPacked} item({100 * (itemPacked / itemNumber).toFixed(2)}%).
       </em>
     </footer>
   );
