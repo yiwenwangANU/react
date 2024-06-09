b9b33e060bcf57fbe75a3017ffb6f0eb796cc1b9
admin01 <paradoxraphael@gmai.com>
2024-06-09 11:21:25 +1000

    challenge init, add slide bar to the component

---
+++ challenge_06_02/src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  const [step, setStep] = useState(4);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  return (
+    <>
+      <div class="slidecontainer">
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          class="slider"
+          id="myRange"
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
+      </div>
+      <div className="countcontainer">
+        <button onClick={() => setCount(count - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount(+count + +step)}>+</button>
+      </div>
+      <div className="messagecontainer">
+        <p>
+          {count < 0 && `${-count} days ago is `}
+          {count === 0 && "today is "}
+          {count > 0 && `${count} days from today is `}
+          {date.toDateString()}
+        </p>
+      </div>
+    </>
+  );
+}
+export default App;
