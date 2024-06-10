8bdcdf9e2881fc3e3956b0f96741702af441cf23
admin01 <paradoxraphael@gmai.com>
2024-06-10 16:37:12 +1000

    update the accordion component

--- exercise_07_1/src/App.js
+++ exercise_07_1/src/App.js
@@ -24,25 +24,42 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [openedItem, setOpenedItem] = useState(null);
+
+  function handleOpen(num) {
+    setOpenedItem(num);
+  }
+
   return (
     <div className="accordion">
       {data.map((e, i) => (
-        <AccordionItem title={e.title} text={e.text} num={i} />
+        <AccordionItem
+          title={e.title}
+          text={e.text}
+          num={i}
+          openedItem={openedItem}
+          handleOpen={handleOpen}
+        />
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [expand, setExpand] = useState(false);
+function AccordionItem({ num, title, text, openedItem, handleOpen }) {
+  // const [expand, setExpand] = useState(false);
   return (
-    <div className={`item ${expand ? "open" : ""}`}>
+    <div className={`item ${openedItem === num ? "open" : ""}`}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon" onClick={(e) => setExpand(!expand)}>
-        {expand ? "-" : "+"}
+      <p
+        className="icon"
+        onClick={(e) =>
+          openedItem === num ? handleOpen(null) : handleOpen(num)
+        }
+      >
+        {openedItem === num ? "-" : "+"}
       </p>
-      {expand ? <div className="content-box">{text}</div> : null}
+      {openedItem === num ? <div className="content-box">{text}</div> : null}
     </div>
   );
 }
