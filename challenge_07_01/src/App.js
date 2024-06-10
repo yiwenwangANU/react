879e7ea9b2883f2b4f64d53eb89cd8d91195521e
admin01 <paradoxraphael@gmai.com>
2024-06-10 17:54:53 +1000

    challenge init

---
+++ challenge_07_01/src/App.js
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import "./App.css";
+
+const satisfaction = [
+  {
+    bill: 0,
+    feel: "Dissatisfied",
+  },
+  {
+    bill: 5,
+    feel: "It was okay",
+  },
+  {
+    bill: 10,
+    feel: "It was good",
+  },
+  {
+    bill: 20,
+    feel: "Absolutely amazing!",
+  },
+];
+
+function App() {
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
+  return (
+    <>
+      <BillCalculator bill={bill} setBill={setBill} />
+      <LikeService
+        message="How did you like the service?"
+        tip={tip}
+        setTip={setTip}
+      />
+      <LikeService
+        message="How did your friend like the service?"
+        tip={friendTip}
+        setTip={setFriendTip}
+      />
+      <Message bill={bill} tip={tip} friendTip={friendTip} />
+      <Reset setBill={setBill} setTip={setTip} setFriendTip={setFriendTip} />
+    </>
+  );
+}
+
+function BillCalculator({ bill, setBill }) {
+  return (
+    <div>
+      <span>How much was the bill?</span>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function LikeService({ message, tip, setTip }) {
+  return (
+    <div>
+      <span>{message}</span>
+      <select value={tip} onChange={(e) => setTip(e.target.value)}>
+        {satisfaction.map((ele) => (
+          <option value={ele.bill}>
+            {ele.feel} ({ele.bill}%)
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function Message({ bill, tip, friendTip }) {
+  const finalTip = (Number(tip) + Number(friendTip)) / 2;
+  const totalBill = (finalTip / 100 + 1) * Number(bill);
+  return <h1>You pay ${totalBill}</h1>;
+}
+
+function Reset({ setBill, setTip, setFriendTip }) {
+  return (
+    <button
+      onClick={(e) => {
+        setBill(0);
+        setTip(0);
+        setFriendTip(0);
+      }}
+    >
+      Reset
+    </button>
+  );
+}
+export default App;
