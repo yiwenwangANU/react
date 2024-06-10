594571cd4469d52083c771905c379d4024ed5d0f
admin01 <paradoxraphael@gmai.com>
2024-06-10 11:44:03 +1000

    accordion component

---
+++ exercise_07_1/src/App.js
@@ -0,0 +1,48 @@
+import "./index.css";
+import { useState } from "react";
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((e, i) => (
+        <AccordionItem title={e.title} text={e.text} num={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [expand, setExpand] = useState(false);
+  return (
+    <div className={`item ${expand ? "open" : ""}`}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon" onClick={(e) => setExpand(!expand)}>
+        {expand ? "-" : "+"}
+      </p>
+      {expand ? <div className="content-box">{text}</div> : null}
+    </div>
+  );
+}
