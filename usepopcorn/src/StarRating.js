bde872bbe0c5d77040716fec99bc180f8c610ef5
admin01 <paradoxraphael@gmai.com>
2024-06-13 15:51:36 +1000

    add propTypes, if the app needs strictly typed use TypeScript

--- usepopcorn/src/StarRating.js
+++ usepopcorn/src/StarRating.js
@@ -1,8 +1,17 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = { display: "flex", alignItems: "center", gap: "16px" };
 const starContainerStyle = { display: "flex" };
 
+StarRating.protoType = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  message: PropTypes.array,
+  onSetRating: PropTypes.func,
+};
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
