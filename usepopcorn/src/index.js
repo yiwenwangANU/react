773dc2ab17d78d98976836809476798261e84700
admin01 <paradoxraphael@gmai.com>
2024-06-16 09:09:21 +1000

    setState should not be in the rendering logic to avoid side effect and infinite loop, use useEffect to specify those side effects that only waht to be executed byonce

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./App_v1";
 import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
+    <App />
+    {/* <StarRating maxRating={5} />
     <StarRating
       maxRating={5}
       size={24}
       message={["Terrible", "Bad", "Okey", "Good", "Amazing"]}
       defaultRating={3}
-    />
+    /> */}
   </React.StrictMode>
 );
