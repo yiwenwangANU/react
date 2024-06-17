d6d19f5c0d22af881dada2b327f884621cfcccb0
admin01 <paradoxraphael@gmai.com>
2024-06-17 11:11:04 +1000

    init commit

---
+++ challenge_12_1/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function App() {
+  const [currencyFrom, setCurrencyFrom] = useState("USD");
+  const [currencyTo, setCurrencyTo] = useState("EUR");
+  const [amount, setAmount] = useState(0);
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
