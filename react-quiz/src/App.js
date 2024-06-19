fdae9c719dc77ce8ee803692570569006099296f
admin01 <paradoxraphael@gmai.com>
2024-06-19 14:57:22 +1000

    loading data from api through useEffect and set the state with useReducer

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,8 +1,49 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  // 'loading','error','ready', 'active','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived": {
+      return { ...state, questions: action.payload, status: "ready" };
+    }
+    case "dataFailed": {
+      return { ...state, status: "error" };
+    }
+    default: {
+      throw new Error("Unknown action type");
+    }
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
