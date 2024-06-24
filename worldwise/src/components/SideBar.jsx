d1ad3c54cf5ef8593f1d2143feb433dac90d8c0d
admin01 <paradoxraphael@gmai.com>
2024-06-24 10:21:21 +1000

    use nested route and index route to manage the state under /app

--- worldwise/src/components/SideBar.jsx
+++ worldwise/src/components/SideBar.jsx
@@ -1,12 +1,13 @@
 import styles from "./SideBar.Module.css";
 import Logo from "../components/Logo";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 function SideBar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
