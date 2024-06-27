e9f19313232acb668a1e7999b3e7c46c746d0c17
admin01 <paradoxraphael@gmai.com>
2024-06-27 10:58:02 +1000

    use context to build city view

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { useCity } from "../contexts/CitiesContext";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 
@@ -9,11 +10,14 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCity();
   const { cityName, emoji, date, id, position } = city;
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -6,6 +6,7 @@ const BASE_URL = "http://localhost:9000";
 function CityProvider({ children }) {
   const [cities, setcities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -16,15 +17,29 @@ function CityProvider({ children }) {
 
         setcities(data);
       } catch (err) {
-        throw new Error(err);
+        throw new Error("Failed to fetch cities:", err);
       } finally {
         setIsLoading(false);
       }
     }
     fetchCities();
   }, []);
+
+  async function getCity(id) {
+    const cityUrl = `${BASE_URL}/cities/${id}`;
+    try {
+      setIsLoading(true);
+      const res = await fetch(cityUrl);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      throw new Error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
-    <CityContext.Provider value={{ cities, isLoading }}>
+    <CityContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CityContext.Provider>
   );
@@ -32,7 +47,8 @@ function CityProvider({ children }) {
 
 function useCity() {
   const context = useContext(CityContext);
-
+  if (context === undefined)
+    throw new Error("CityContext was used outside CityProvider");
   return context;
 }
 
