07671bc228d447e5315f578dcaa0f6d8bdbcdd7d
admin01 <paradoxraphael@gmai.com>
2024-06-29 09:45:42 +1000

    include map using react leaflet

--- worldwise/src/components/Form.jsx
+++ worldwise/src/components/Form.jsx
@@ -15,7 +15,7 @@ export function convertToEmoji(countryCode) {
 
 function Form() {
   const [cityName, setCityName] = useState("");
-  const [country, setCountry] = useState("");
+
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
 
