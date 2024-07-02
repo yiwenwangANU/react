67d31ed57bf33c9465795c0e272acc3447e00cd7
admin01 <paradoxraphael@gmai.com>
2024-07-02 14:47:20 +1000

    add route protection

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -12,6 +12,7 @@ import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 import { CityProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -24,7 +25,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
 
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
               <Route path="countries" element={<CountryList />} />
