847c610038d6fd7918f1135d840b742f0a5d1fa7
admin01 <paradoxraphael@gmai.com>
2024-07-04 15:12:23 +1000

    using action creator

--- redux-intro/src/Store.js
+++ redux-intro/src/Store.js
@@ -31,10 +31,21 @@ const reducer = (state = initialState, action) => {
 };
 
 const store = createStore(reducer);
-store.dispatch({ type: "account/deposite", payload: 500 });
-console.log(store.getState());
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a Car" },
-});
+
+function deposite(amount) {
+  return { type: "account/deposite", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+store.dispatch(requestLoan(500, "buy a car"));
 console.log(store.getState());
