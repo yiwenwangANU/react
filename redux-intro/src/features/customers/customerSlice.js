47de58496e21578c54965661c7faa7a1a429ee88
admin01 <paradoxraphael@gmai.com>
2024-07-06 17:41:50 +1000

    implement RTK to customerSlice

--- redux-intro/src/features/customers/customerSlice.js
+++ redux-intro/src/features/customers/customerSlice.js
@@ -1,44 +1,36 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: null,
   nationalID: null,
   createdAt: null,
 };
 
-const customerReducer = (state = initialStateCustomer, action) => {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-      };
-    default:
-      return state;
-  }
-};
-
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName: fullName,
-      nationalID: nationalID,
-      createdAt: new Date().toISOString(),
+const customerSlice = createSlice({
+  name: "customer",
+  initialState: initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
     },
-  };
-}
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-export function updateName(fullName) {
-  return {
-    type: "customer/updateName",
-    payload: fullName,
-  };
-}
-
-export default customerReducer;
+export default customerSlice.reducer;
+export const { createCustomer, updateName } = customerSlice.actions;
